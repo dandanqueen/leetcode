80e657847ab89e168c7487a9363f91e8d37639ba
zhaoj62 <13928441612@163.com>
2018-03-01 23:20:16 +0800

    Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        char appear[255] = {0};
-        int position[255];
+        char appear[255] = {0}; /*记录字符出现情况*/
+        int position[255]; /*记录字符出现位置*/
         int len = s.length();
         int max = 0;
         int sublen = 0;
@@ -10,16 +10,17 @@ public:
             if (appear[s[i]]) {
                 if (sublen > max)
                     max = sublen;
-                sublen = i - position[s[i]] - 1;
-                for (int k = j;k <= position[s[i]];k ++){
+                for (int k = j;k <= position[s[i]];k ++){ /*重置前无效部分，重新开始需找*/
                     appear[s[k]] = 0;
                 }
+                sublen = i - position[s[i]] - 1; /*从重复字符串后一个字符串开始需找下一个子字符串*/
+                j = position[s[i]] + 1; /*下一个子字符串开始位置*/
             }
             position[s[i]] = i;
             appear[s[i]] = 1;
             sublen ++;
         }
-        if (sublen > max)
+        if (sublen > max) /*直到字符串结束任然没有找到重复字符情况*/
             max = sublen;
         return max;
     }
